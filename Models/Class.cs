e4b8dc900aefdf45ee6084f48cccda90c71035cd
siddharthakc <siddharthakc36@gmail.com>
2024-03-18 07:51:10 +0545

    Add project files.

---
+++ Models/Class.cs
@@ -0,0 +1,17 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace MyLearning.Models
+{
+    public static class DatabaseContext
+    {
+        public const string ConnectionString = "User Id=sid;Password=sid;Data Source=localhost:1521/orcl;";
+
+        public static OracleConnection GetOpenConnection()
+        {
+            OracleConnection connection = new OracleConnection(ConnectionString);
+            connection.Open();
+            return connection;
+        }
+
+    }
+}
